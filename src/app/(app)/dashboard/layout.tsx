210bb88b211c1a5646cecacb3592a939a4bf9747
viyaga <mohan2609kumar@gmail.com>
2025-06-21 13:18:42 +0530

    dashboard

--- src/app/(app)/dashboard/layout.tsx
+++ src/app/(app)/dashboard/layout.tsx
@@ -46,20 +46,20 @@ export default async function RootLayout({
                     resources={[
                       {
                         name: "blog_posts",
-                        list: "/blog-posts",
-                        create: "/blog-posts/create",
-                        edit: "/blog-posts/edit/:id",
-                        show: "/blog-posts/show/:id",
+                        list: "/dashboard/blog-posts",
+                        create: "/dashboard/blog-posts/create",
+                        edit: "/dashboard/blog-posts/edit/:id",
+                        show: "/dashboard/blog-posts/show/:id",
                         meta: {
                           canDelete: true,
                         },
                       },
                       {
                         name: "categories",
-                        list: "/categories",
-                        create: "/categories/create",
-                        edit: "/categories/edit/:id",
-                        show: "/categories/show/:id",
+                        list: "/dashboard/categories",
+                        create: "/dashboard/categories/create",
+                        edit: "/dashboard/categories/edit/:id",
+                        show: "/dashboard/categories/show/:id",
                         meta: {
                           canDelete: true,
                         },
