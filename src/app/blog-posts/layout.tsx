7b326b37a9df2a32d1761f17d292daecdb54c06c
viyaga <mohan2609kumar@gmail.com>
2025-06-21 07:16:53 +0530

    init

---
+++ src/app/blog-posts/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { ThemedLayoutV2 } from "@refinedev/antd";
+import { Header } from "@components/header";
+
+export default async function Layout({ children }: React.PropsWithChildren) {
+  return <ThemedLayoutV2 Header={Header}>{children}</ThemedLayoutV2>;
+}
